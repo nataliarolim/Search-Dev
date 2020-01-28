aa77de3979fde3728ca2ac23b24049bce8a8c467
Natália Rolim <natalia.rolim@celfocus.com>
2020-01-28 18:57:45 +0000

    create post route

---
+++ backend/src/routes.js
@@ -0,0 +1,26 @@
+const {Router} = require('express');
+const routes = Router();
+const axios = require('axios');
+const Dev = require('./models/Dev');
+
+routes.post('/devs', async (request, response) => {
+    const {github_username, techs} = request.body;
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+
+    //console.log(apiResponse);
+    const {name = login, avatar_url, bio} = apiResponse.data;
+
+    const techsArray = techs.split(',').map(tech => tech.trim());
+
+    const dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs : techsArray,
+    });
+
+    return response.json(dev);
+});
+
+module.exports = routes;
